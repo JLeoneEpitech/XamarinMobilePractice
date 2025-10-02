4d5e24e4b22fa2ec1d7caaa7fd9c7af74d625e66
Jonathan Leone <jonathan.leone@epitech.eu>
2025-10-02 13:52:19 +0200

    Create class Apiclient

---
+++ Day1ApiClient/apiClient.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class ApiClient : IDisposable
+{
+
+    private readonly HttpClient _http = new HttpClient();
+
+    public async Task<List<T>> GetAsync<T>(string url)
+    {
+        try
+        {
+            using var resp = await _http.GetAsync(url); //GET
+            resp.EnsureSuccessStatusCode(); //S'assurer que le statuscode commence par 2xx - Possilbe aussi de faire resp.IsSuccessStatusCode  == 200 et gérer les exceptions
+            var json = await resp.Content.ReadAsStringAsync(); //GET the json
+            var opts = new JsonSerializerOptions { PropertyNameCaseInsensitive = true }; //set the options here no case sensitive
+            var result = JsonSerializer.Deserialize<List<T>>(json, opts); //déserialize the result to be used as a list of T
+            return result ?? new List<T>(); //return result or list by default if error - can be upgrade later
+
+        }
+        catch (Exception)
+        {
+            return new List<T>();
+        }
+
+    }
+    public void Dispose() => _http.Dispose();
+}
